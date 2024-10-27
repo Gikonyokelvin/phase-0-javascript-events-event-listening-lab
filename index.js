20bc4298d9a8a99e60abf87e1f836c0989710888
Kelvin Gikonyo <gikonyosenior@gmail.com>
2024-10-27 16:18:47 +0300

    complete events lab

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 function addingEventListener() {
+    const button = document.getElementById('button');
+    button.addEventListener('click', () => {
+        const main = document.getElementById('main');
+        main.textContent = 'My ID is "main"!';
+    });
 }
